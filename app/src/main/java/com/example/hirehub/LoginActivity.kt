43f7ec7ea9ca9643de21cba9ec1ef61a18b25196
ssoleil <polina.rohoza@gmail.com>
2023-03-02 21:04:46 +0200

    adding binding and logic to login and register

--- app/src/main/java/com/example/hirehub/LoginActivity.kt
+++ app/src/main/java/com/example/hirehub/LoginActivity.kt
@@ -1,11 +1,39 @@
 package com.example.hirehub
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hirehub.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            if (binding.etUsername.text.isEmpty()) {
+                binding.etUsername.error = "Username is required!"
+            }
+            else if (binding.etPwd.text.isEmpty()) {
+                binding.etUsername.error = "Password is required"
+            } else {
+                // todo: check before
+                val i = Intent(applicationContext, MainActivity::class.java)
+                startActivity(i)
+            }
+        }
+
+        binding.tvRegister.setOnClickListener {
+            // todo: check before
+            val i = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
--- app/src/main/java/com/example/hirehub/RegisterActivity.kt
+++ app/src/main/java/com/example/hirehub/RegisterActivity.kt
@@ -1,11 +1,38 @@
 package com.example.hirehub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hirehub.databinding.ActivityLoginBinding
+import com.example.hirehub.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            if (binding.etName.text.isEmpty()) {
+                binding.etName.error = "Name is required!"
+            } else if (binding.etUsername.text.isEmpty()) {
+                binding.etUsername.error = "Username is required"
+            } else if (binding.etPwd.text.isEmpty()) {
+                binding.etPwd.error = "Password is required"
+            } else {
+                val i = Intent(applicationContext, MainActivity::class.java)
+                startActivity(i)
+            }
+        }
+
+        binding.tvLogin.setOnClickListener {
+            val i = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
