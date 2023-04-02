0bdd2ed45d3627c2bd0642dee4bbc10733a6cbe0
biaOkVQM <113008015+MykhailoMusiienkoMosig@users.noreply.github.com>
2023-04-02 16:39:13 +0200

    Views (HR_request, Seeker_account)

---
+++ app/src/main/java/com/example/hirehub/AccountSeekerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hirehub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountSeekerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_seeker)
+    }
+}
